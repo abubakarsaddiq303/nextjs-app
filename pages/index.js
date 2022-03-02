74f0244c9646e4a4af10929ac6a7306d1b5bef1d
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2022-03-02 15:48:31 +0500

    new

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,10 @@ import Features from "../components/Features";
 import Hero from "../components/Hero";
 import Roadmap from "../components/Roadmap";
 import Team from "../components/Team";
-import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      {/* <Header /> */}
       <Hero />
       <Team />
       <Features />
